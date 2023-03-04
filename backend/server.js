dfb5cbb7e4aa023571ed57171cef3df24c194b22
Faisal El-Mutwalli <f709@outlook.com>
2023-03-03 23:02:56 -0500

    debugging db connection

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const dbName = 'authentication';
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_SRC}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(client)
   try {
     client.connect().then(() => {
 
